732318e9bb6ed8994adcf3dad983fa1dae265810
Stephen Chen <sun999@gmail.com>
2017-02-06 18:23:00 -0800

    Fragment Practice

---
+++ Class6_Fragment/app/src/main/java/com/example/stephen/class6_fragment/TopSectionFragment.java
@@ -0,0 +1,21 @@
+package com.example.stephen.class6_fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Stephen on 2/6/2017.
+ */
+
+public class TopSectionFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.top_section_fragment,container,false);
+        return view;
+    }
+}
